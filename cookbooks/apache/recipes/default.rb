21373a7c30de8618bdca13e1e5d6d916fc02fcd7
Glenn Dyess <glenn.dyess@gmail.com>
2018-05-29 16:31:18 +0000

    yes

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
